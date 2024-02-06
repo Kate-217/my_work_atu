e1163261efc607c53ef91c4c610f921447851c3a
Kate Lisovenko <kate.spb@gmail.com>
2024-02-06 12:40:49 +0000

    added lab 2

---
+++ week02.ipynb/hello.py
@@ -0,0 +1,9 @@
+# Lab 2.2
+
+print("hello Kate")
+# multiply py
+answer = 111 * 555
+print(answer)
+
+
+
---
+++ week02.ipynb/hello2.py
@@ -0,0 +1,9 @@
+# the program reads a person’s name and prints it out
+
+name = input("What is your name?")
+
+print('Hello '+ name + '\nNice to meet you')
+
+# or you could do this with format
+
+print(f'Hello {name}\nNice to meet you')
